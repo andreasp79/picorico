26b4cec601ad10dc35a08f02b4c2c201294aa079
andreas <andreasp79@hotmail.ca>
2022-03-11 16:24:39 -0700

    xrick first add with sound & zip disabled

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 
 using namespace picosystem;
 
-void init() {
+void init() 
+{
+    
 }
 
-void update(uint32_t tick) {
+void update(uint32_t tick) 
+{
+
 }
 
-void draw(uint32_t tick) {
+void draw(uint32_t tick) 
+{
+pen(0, 0, 0);
+  clear();
+
+  pen(15, 15, 15);
+  text("xrickie", 0, 0);
 }
